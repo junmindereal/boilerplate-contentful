b85d6117f3d511bf0f73560ab7cc4aefa92ccdc8
Junmin De Real <junmindereal@users.noreply.github.com>
2021-07-01 10:03:04 +0800

    Delete src/components/homepage directory

--- src/components/homepage/index.jsx
+++
@@ -1,9 +0,0 @@
-import { styles } from './styles'
-
-const { heading } = styles
-
-export default function Homepage () {
-  return (
-    <h1 className={heading}>Hello World</h1>
-  )
-}
--- src/components/homepage/styles.js
+++
@@ -1,5 +0,0 @@
-export const styles = {
-  heading: `
-    text-2xl
-  `
-}
